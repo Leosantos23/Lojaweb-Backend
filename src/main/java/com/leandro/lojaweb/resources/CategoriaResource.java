d900721497dfa0f5816ed1edcde4f424740c8c0c
Leandro Moreira <leandroangus@gmail.com>
2021-02-17 10:30:05 -0300

    Criando repository e service para Categoria, Testado!

--- src/main/java/com/leandro/lojaweb/resources/CategoriaResource.java
+++ src/main/java/com/leandro/lojaweb/resources/CategoriaResource.java
@@ -3,27 +3,28 @@ package com.leandro.lojaweb.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandro.lojaweb.domain.Categoria;
+import com.leandro.lojaweb.services.CategoriaService;
 
 @RestController//Anotacao controladora REST
 @RequestMapping(value="/categorias")//Aqui eu coloco o nome do end point REST
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)//Para que este metodo seja REST tenho que associar a algum verbo HTTP (GET, POST, etc)
-	public List<Categoria> listar () {
+	@Autowired//Para instanciar automaticamente
+	private CategoriaService service;
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.GET)//Para que este metodo seja REST tenho que associar a algum verbo HTTP (GET, POST, etc)
+	public ResponseEntity<?> find (@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		Categoria obj = service.buscar(id);//Aqui chamo o obj o service ao metodo  buscar, repassando o id.
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/leandro/lojaweb/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.leandro.lojaweb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leandro.lojaweb.domain.Categoria;
+import com.leandro.lojaweb.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired//Aqui eu instancio o repositorio (repo) abaixo, que na qual sera automaticamente instanciada pelo SPRING.
+	//Pelo mecanismo de injecao de dependencias, ou inversao de controle.
+	private CategoriaRepository repo;//Aqui declaro uma dependencia de um objeto do tipo categoria.
+	
+	//Aqui vou fazer uma funcao de buscar a Categoria por ID.
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
