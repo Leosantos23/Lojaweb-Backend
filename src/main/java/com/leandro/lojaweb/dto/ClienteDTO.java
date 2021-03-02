fffab5a23533cd36b9191f1b71ee52fcfba3d0db
Leandro Moreira <leandroangus@gmail.com>
2021-03-02 11:10:50 -0300

    Validacao customizada, Email nao repetido na atualizacao de Cliente. testada.

--- src/main/java/com/leandro/lojaweb/dto/ClienteDTO.java
+++ src/main/java/com/leandro/lojaweb/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.leandro.lojaweb.domain.Cliente;
+import com.leandro.lojaweb.services.validacao.ClienteUpdate;
 
+@ClienteUpdate//Chamo a anotattion customizada de updade
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
