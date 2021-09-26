0686df491bb6978c60ebc3084575d8f879f747c4
Leosantos23 <leandroangus@gmail.com>
2021-09-26 11:43:13 -0300

    Salvando primeiro arquivo no aws S3

--- src/main/java/com/leandro/lojaweb/LojawebApplication.java
+++ src/main/java/com/leandro/lojaweb/LojawebApplication.java
@@ -1,13 +1,19 @@
 package com.leandro.lojaweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.leandro.lojaweb.services.S3Service;
 
 @SpringBootApplication
 //Implementei a CommandLineRunner para ja ir inserindo dados diretamente ao banco, pois a primeiro insert no commit interior
 //tive de fazer manualmente.
 public class LojawebApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojawebApplication.class, args);
@@ -15,6 +21,8 @@ public class LojawebApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("D:\\Shelby2010.jpg");
 
 	}
 
