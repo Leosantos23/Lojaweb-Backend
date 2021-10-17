40185cddba46e84cc8a48c6d36514c4dac205149
Leosantos23 <leandroangus@gmail.com>
2021-10-17 08:29:44 -0300

    Adicionando mais produtos, para testar infinity scroll

--- src/main/java/com/leandro/lojaweb/services/DBService.java
+++ src/main/java/com/leandro/lojaweb/services/DBService.java
@@ -70,7 +70,7 @@ public class DBService {
 	// Aqui chamo o repositorio
 	@Autowired // Para ser instanciado automaticamente
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
@@ -95,8 +95,95 @@ public class DBService {
 		Produto p9 = new Produto(null, "Abajour", 100.00);
 		Produto p10 = new Produto(null, "Pendente", 180.00);
 		Produto p11 = new Produto(null, "Shampoo", 90.00);
+		
+		// Acrescentando mais produtos para testar infinity scroll
+		Produto p12 = new Produto(null, "Produto 12", 10.00);
+		Produto p13 = new Produto(null, "Produto 13", 10.00);
+		Produto p14 = new Produto(null, "Produto 14", 10.00);
+		Produto p15 = new Produto(null, "Produto 15", 10.00);
+		Produto p16 = new Produto(null, "Produto 16", 10.00);
+		Produto p17 = new Produto(null, "Produto 17", 10.00);
+		Produto p18 = new Produto(null, "Produto 18", 10.00);
+		Produto p19 = new Produto(null, "Produto 19", 10.00);
+		Produto p20 = new Produto(null, "Produto 20", 10.00);
+		Produto p21 = new Produto(null, "Produto 21", 10.00);
+		Produto p22 = new Produto(null, "Produto 22", 10.00);
+		Produto p23 = new Produto(null, "Produto 23", 10.00);
+		Produto p24 = new Produto(null, "Produto 24", 10.00);
+		Produto p25 = new Produto(null, "Produto 25", 10.00);
+		Produto p26 = new Produto(null, "Produto 26", 10.00);
+		Produto p27 = new Produto(null, "Produto 27", 10.00);
+		Produto p28 = new Produto(null, "Produto 28", 10.00);
+		Produto p29 = new Produto(null, "Produto 29", 10.00);
+		Produto p30 = new Produto(null, "Produto 30", 10.00);
+		Produto p31 = new Produto(null, "Produto 31", 10.00);
+		Produto p32 = new Produto(null, "Produto 32", 10.00);
+		Produto p33 = new Produto(null, "Produto 33", 10.00);
+		Produto p34 = new Produto(null, "Produto 34", 10.00);
+		Produto p35 = new Produto(null, "Produto 35", 10.00);
+		Produto p36 = new Produto(null, "Produto 36", 10.00);
+		Produto p37 = new Produto(null, "Produto 37", 10.00);
+		Produto p38 = new Produto(null, "Produto 38", 10.00);
+		Produto p39 = new Produto(null, "Produto 39", 10.00);
+		Produto p40 = new Produto(null, "Produto 40", 10.00);
+		Produto p41 = new Produto(null, "Produto 41", 10.00);
+		Produto p42 = new Produto(null, "Produto 42", 10.00);
+		Produto p43 = new Produto(null, "Produto 43", 10.00);
+		Produto p44 = new Produto(null, "Produto 44", 10.00);
+		Produto p45 = new Produto(null, "Produto 45", 10.00);
+		Produto p46 = new Produto(null, "Produto 46", 10.00);
+		Produto p47 = new Produto(null, "Produto 47", 10.00);
+		Produto p48 = new Produto(null, "Produto 48", 10.00);
+		Produto p49 = new Produto(null, "Produto 49", 10.00);
+		Produto p50 = new Produto(null, "Produto 50", 10.00);
+		
+		// Associo os produtos novos a categoria 1 para ester infinity scroll
+		cat1.getProdutos().addAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
+		// Instanciacao de produtos novos a categoria 1
+		p12.getCategorias().add(cat1);
+		p13.getCategorias().add(cat1);
+		p14.getCategorias().add(cat1);
+		p15.getCategorias().add(cat1);
+		p16.getCategorias().add(cat1);
+		p17.getCategorias().add(cat1);
+		p18.getCategorias().add(cat1);
+		p19.getCategorias().add(cat1);
+		p20.getCategorias().add(cat1);
+		p21.getCategorias().add(cat1);
+		p22.getCategorias().add(cat1);
+		p23.getCategorias().add(cat1);
+		p24.getCategorias().add(cat1);
+		p25.getCategorias().add(cat1);
+		p26.getCategorias().add(cat1);
+		p27.getCategorias().add(cat1);
+		p28.getCategorias().add(cat1);
+		p29.getCategorias().add(cat1);
+		p30.getCategorias().add(cat1);
+		p31.getCategorias().add(cat1);
+		p32.getCategorias().add(cat1);
+		p33.getCategorias().add(cat1);
+		p34.getCategorias().add(cat1);
+		p35.getCategorias().add(cat1);
+		p36.getCategorias().add(cat1);
+		p37.getCategorias().add(cat1);
+		p38.getCategorias().add(cat1);
+		p39.getCategorias().add(cat1);
+		p40.getCategorias().add(cat1);
+		p41.getCategorias().add(cat1);
+		p42.getCategorias().add(cat1);
+		p43.getCategorias().add(cat1);
+		p44.getCategorias().add(cat1);
+		p45.getCategorias().add(cat1);
+		p46.getCategorias().add(cat1);
+		p47.getCategorias().add(cat1);
+		p48.getCategorias().add(cat1);
+		p49.getCategorias().add(cat1);
+		p50.getCategorias().add(cat1);	
 
-		// aqui faco as associacoes das categorias
+		// Aqui faco as associacoes das categorias
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
 		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
@@ -105,7 +192,7 @@ public class DBService {
 		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
 		cat7.getProdutos().addAll(Arrays.asList(p11));
 
-		// aqui faco as associacoes dos produtos
+		// Aqui faco as associacoes dos produtos
 		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
 		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
@@ -118,10 +205,16 @@ public class DBService {
 		p10.getCategorias().addAll(Arrays.asList(cat6));
 		p11.getCategorias().addAll(Arrays.asList(cat7));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));// Aqui salva todas
-																								// categorias
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));// Aqui salva todos
-																								// produtos
+		// Aqui grava todas categorias no banco de dados.
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+
+		// Aqui grava todos produtos no banco de dados.
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+		
+		// Aqui grava os produtos novos no banco de dados
+		produtoRepository.saveAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
 
 		// Aqui faco as instancias dos estados
 		Estado est1 = new Estado(null, "Minas Gerais");
@@ -142,15 +235,16 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		// Instancias de Clientes
-		Cliente cli1 = new Cliente(null, "Leandro Moreira", "lesoft.com@gmail.com", "97778845632", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Leandro Moreira", "lesoft.com@gmail.com", "97778845632",
+				TipoCliente.PESSOAFISICA, pe.encode("123"));
 		// Aqui instancio os telefones do cliente
 		cli1.getTelefones().addAll(Arrays.asList("999090935", "992213331"));
-		
-		Cliente cli2 = new Cliente(null, "Deisa Machado", "deisa.com@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+
+		Cliente cli2 = new Cliente(null, "Deisa Machado", "deisa.com@gmail.com", "31628382740",
+				TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli2.getTelefones().addAll(Arrays.asList("999090933", "992213333"));// Aqui instancio os telefones do cliente
 		cli2.addPerfil(Perfil.ADMIN);// Adicionar como admim
-		
-		
+
 		// Instancias de endereco
 		Endereco e1 = new Endereco(null, "Rua rp10", "s/n", "Qd 21, Lt 06", "Jardim Paraiso", "75456224", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua rp13", "s/n", "Qd 20, Lt 26", "Jardim Flores", "75454724", cli1, c2);
@@ -159,7 +253,7 @@ public class DBService {
 		// Associar o cliente tem que conhecer o endereco dele
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		cli2.getEnderecos().addAll(Arrays.asList(e3));
-		
+
 		// Salvar os cliente no banco de dados
 		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
 
